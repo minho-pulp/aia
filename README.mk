e7de67e9d4a36bdea8c7d8e4355526894b177a1f
Francisco Marques <55896020+D3boker1@users.noreply.github.com>
2023-07-01 10:39:52 +0100

    Update and rename README.org to README.mk

--- README.mk
+++ README.mk
@@ -1,10 +1,11 @@
-#+TITLE: Advanced Interrupt Architecture Implementation
-#+AUTHOR: Francisco Marques
-#+EMAIL: fmarques_00@protonmail.com
-#+STARTUP: show2levels
+# Advanced Interrupt Architecture Implementation
 
 New project location is [zero-day-labs riscv-aia](https://github.com/zero-day-labs/riscv-aia)
 
-* Disclaimer
+## Disclaimer
 We would like to inform you that this repository is now discontinued and has been moved permanently to a new location. Going forward, all development and updates will be taking place at our new repository located at: https://github.com/zero-day-labs/riscv-aia
 Please note that this current repository will no longer receive updates or bug fixes, and we encourage you to visit the new repository to stay up-to-date with the latest developments. Feel free to explore the new repository and engage with the community there. We welcome your feedback, contributions, and any issues you may encounter.
+
+## Contacts
+
+Francisco Marques (D3boker1): fmarques_00@protonmail.com
